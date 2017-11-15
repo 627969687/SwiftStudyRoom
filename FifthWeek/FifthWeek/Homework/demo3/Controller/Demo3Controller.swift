25338fc2a4da1028dbe5518c460cab67ea766c4b
Jason <627969687@qq.com>
2017-11-15 16:45:59 +0800

    fifth week update

--- FifthWeek/FifthWeek/Homework/demo3/Controller/Demo3Controller.swift
+++ FifthWeek/FifthWeek/Homework/demo3/Controller/Demo3Controller.swift
@@ -18,7 +18,7 @@ class Demo3Controller: UIViewController {
 extension Demo3Controller {
     fileprivate func setupUI() {
         view.backgroundColor = UIColor.black
-        print("test")
+        
         let margin:CGFloat = 100
         let loadingV = LoadingView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: screenW - margin, height: screenW - margin)), colors: [#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1),#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1),#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)])
         loadingV.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 0)
